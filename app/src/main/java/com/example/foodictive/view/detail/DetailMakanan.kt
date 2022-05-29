e7f8354091adc47ac6dafec626faeb2a1db71e9b
irfan201 <82924589+irfan201@users.noreply.github.com>
2022-05-29 12:45:39 +0700

    feat: update detail

--- app/src/main/java/com/example/foodictive/view/detail/DetailMakanan.kt
+++ app/src/main/java/com/example/foodictive/view/detail/DetailMakanan.kt
@@ -1,26 +1,65 @@
 package com.example.foodictive.view.detail
 
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
+import com.example.foodictive.MainActivity.Companion.CAMERA_RESULT
 import com.example.foodictive.R
 import com.example.foodictive.databinding.ActivityDetailMakananBinding
 import com.example.foodictive.response.Food
+import com.example.foodictive.uriToFile
+import java.io.File
 
 class DetailMakanan : AppCompatActivity() {
     private lateinit var detailModel: DetailModel
     private lateinit var binding: ActivityDetailMakananBinding
+    private var getFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailMakananBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.identifikasi.setOnClickListener {
-            setupViewModel()
+        getPhoto()
+        binding.identifikasi.setOnClickListener { setupViewModel() }
+        binding.addGalery.setOnClickListener { startGalery() }
+
+
+
+    }
+
+    private fun getPhoto() = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == CAMERA_RESULT){
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            getFile = myFile
+            val result = BitmapFactory.decodeFile(myFile.path)
+
+            binding.imageDetail.setImageBitmap(result)
+
         }
+    }
 
+    private fun startGalery(){
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent,"Choose a picture")
+        lancuhIntentGalery.launch(chooser)
+    }
 
-
+    private val lancuhIntentGalery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        if (it.resultCode == RESULT_OK){
+            val selectedImg: Uri = it.data?.data as Uri
+            val myFile = uriToFile(selectedImg,this)
+            getFile = myFile
+            binding.imageDetail.setImageURI(selectedImg)
+        }
     }
 
     private fun setupViewModel(){
