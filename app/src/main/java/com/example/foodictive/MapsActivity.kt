25f02d1fb32decdcb6bd3653515c7102b0d34e7f
Gian Satya <giansatya5@gmail.com>
2022-06-06 13:50:43 +0700

    Fix Map Fun
    Fix Map Fun

--- app/src/main/java/com/example/foodictive/MapsActivity.kt
+++ app/src/main/java/com/example/foodictive/MapsActivity.kt
@@ -1,15 +1,18 @@
 package com.example.foodictive
 
-
 import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.example.foodictive.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -40,8 +43,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         getMyLocation()
     }
 
-
-
     private val requestPermissionLauncher =
         registerForActivityResult(
             ActivityResultContracts.RequestPermission()
@@ -50,6 +51,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 getMyLocation()
             }
         }
+
     private fun getMyLocation() {
         if (ContextCompat.checkSelfPermission(
                 this.applicationContext,
